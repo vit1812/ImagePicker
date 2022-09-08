c16122ec053724afd66a88d233430f1d6313feba
Luong Vinh <vinhlt1812@gmail.com>
2022-09-08 17:53:28 +0700

    add image bundle to package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -20,10 +20,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ImagePicker",
-            dependencies: [],
-            path: "Source")
-    ],
-    swiftLanguageVersions: [.v5]
+        .target(name: "ImagePicker",
+                path: "Source",
+                resources: [
+                    .process("Images")
+                ])
+    ]
 )
